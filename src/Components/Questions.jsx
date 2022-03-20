18cf784e5a5b0b4711eda8a8dd69894cd85b2b5a
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-20 14:05:10 -0300

    Refactores Questions.jsx

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -2,137 +2,84 @@ import React, { useState } from "react";
 import "./Styles/Questions.css";
 import { HashLink as Link } from "react-router-hash-link";
 
+const questionsList = [
+  {
+    quest: "Para que serve a extensão?",
+    res: "Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor  dignissim tellus in malesuada. Fusce bibendum libero est, quis  vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin   sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui",
+  },
+  {
+    quest: "Como posso solicitar para um novo navegador?",
+    res: "Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor  dignissim tellus in malesuada. Fusce bibendum libero est, quis  vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin   sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui",
+  },
+  {
+    quest: "Existe uma versão mobile?",
+    res: "Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor  dignissim tellus in malesuada. Fusce bibendum libero est, quis  vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin   sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui",
+  },
+  {
+    quest: "E quanto a outros navegadores Chromium?",
+    res: "Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor  dignissim tellus in malesuada. Fusce bibendum libero est, quis  vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin   sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui",
+  },
+];
+
 function Questions() {
-  const [num, setNum] = useState(0);
+  const [num, setNum] = useState(-1);
 
   return (
     <>
       <section className="questions-section">
         <div className="questions-texts-container">
+          {/* SECTION TITLE */}
           <h2 data-aos="fade-down" data-aos-offset="300">
             Perguntas frequentes
           </h2>
+
           <p
             data-aos="fade-down"
             data-aos-offset="300"
             className="question-header-text"
           >
-            Aqui estão algumas de nossas perguntas mais frequentes. Se você tiver outras perguntas
-            sinta-se à vontade para nos enviar um e-mail.
+            Aqui estão algumas de nossas perguntas mais frequentes. Se você
+            tiver outras perguntas sinta-se à vontade para nos enviar um e-mail.
           </p>
-          <div className="question-container">
-            <hr />
-            <div className="button-container">
-              <button
-                onClick={() => {
-                  if (num == 1) {
-                    setNum(-1);
-                  } else {
-                    setNum(1);
-                  }
-                }}
-              >
-                Para que serve a extensão?
-              </button>
-              <i
-                className={`fas fa-chevron-${num == 1 ? "up red" : "down"}`}
-              ></i>
-            </div>
-            <p className={`question-text ${num == 1 ? "opened" : null}`}>
-              Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor
-              dignissim tellus in malesuada. Fusce bibendum libero est, quis
-              vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.
-              Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin
-              sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui
-            </p>
-            <hr />
-          </div>
 
-          <div className="question-container">
-            <div className="button-container">
-              <button
-                onClick={() => {
-                  if (num == 2) {
-                    setNum(-1);
-                  } else {
-                    setNum(2);
-                  }
-                }}
-              >
-                Como posso solicitar para um novo navegador?
-              </button>
-              <i
-                className={`fas fa-chevron-${num == 2 ? "up red" : "down"}`}
-              ></i>
+          {/* QUESTIONS ITEMS */}
+          {questionsList.map((item, index) => (
+            <div className="question-container" key={index}>
+              <hr />
+              <div className="button-container">
+                <button
+                  // IF INDEX === USESTATE INDEX IT OPENS
+                  onClick={() => {
+                    if (num === index) {
+                      setNum(-1);
+                    } else {
+                      setNum(index);
+                    }
+                  }}
+                >
+                  {/* QUESTION TITLE */}
+                  {item.quest}
+                </button>
+                <i
+                  className={`fas fa-chevron-${
+                    num === index ? "up red" : "down"
+                  }`}
+                ></i>
+              </div>
+              <p className={`question-text ${num === index ? "opened" : null}`}>
+                {/* QUESTION RESPONSE */}
+                {item.res}
+              </p>
             </div>
-            <p className={`question-text ${num == 2 ? "opened" : null}`}>
-              Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor
-              dignissim tellus in malesuada. Fusce bibendum libero est, quis
-              vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.
-              Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin
-              sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui
-            </p>
-            <hr />
-          </div>
-
-          <div className="question-container">
-            <div className="button-container">
-              <button
-                onClick={() => {
-                  if (num == 3) {
-                    setNum(-1);
-                  } else {
-                    setNum(3);
-                  }
-                }}
-              >
-                Existe uma versão mobile?
-              </button>
-              <i
-                className={`fas fa-chevron-${num == 3 ? "up red" : "down"}`}
-              ></i>
-            </div>
-            <p className={`question-text ${num == 3 ? "opened" : null}`}>
-              Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor
-              dignissim tellus in malesuada. Fusce bibendum libero est, quis
-              vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.
-              Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin
-              sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui
-            </p>
-            <hr />
-          </div>
-
-          <div className="question-container">
-            <div className="button-container">
-              <button
-                onClick={() => {
-                  if (num == 4) {
-                    setNum(-1);
-                  } else {
-                    setNum(4);
-                  }
-                }}
-              >
-                E quanto a outros navegadores Chromium?
-              </button>
-              <i
-                className={`fas fa-chevron-${num == 4 ? "up red" : "down"}`}
-              ></i>
-            </div>
-            <p className={`question-text ${num == 4 ? "opened" : null}`}>
-              Aliquam erat volutpat. Praesent vitae ex nulla. Nullam porttitor
-              dignissim tellus in malesuada. Fusce bibendum libero est, quis
-              vulputate ipsum semper quis. Phasellus facilisis vehicula blandit.
-              Sed a malesuada purus. Fusce rutrum, massa vel sollicitudin
-              sollicitudin, nisi nisl lobortis nisi, eget scelerisque nulla dui
-            </p>
-            <hr />
-          </div>
+          ))}
+          <hr />
         </div>
+
+        {/* "ADICIONAR" BUTTON*/}
         <div className="moreInfo">
-        <Link to="#download">
-                <button className="questions-add-btn">ADICIONAR</button>
-        </Link>
+          <Link to="#download">
+            <button className="questions-add-btn">ADICIONAR</button>
+          </Link>
         </div>
       </section>
     </>
