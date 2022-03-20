51739b9154142924cd2cfd9a0762fd2eb54d36d2
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-20 15:59:18 -0300

    Refactored Features.jsx

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -2,142 +2,101 @@ import React, { useState } from "react";
 import "./Styles/Features.css";
 import { HashLink as Link } from "react-router-hash-link";
 
-function Features() {
-  const [SBactivated, setSBActivated] = useState(true);
-  const [SSactivated, setSSActivated] = useState(false);
-  const [ESactivated, setESActivated] = useState(false);
+const changeBTNS = [
+  {
+    title: "Simple Bookmarking",
+    fadeType: "fade-right",
+  },
+  {
+    title: "Pesquisa rápida",
+    fadeType: "",
+  },
+  {
+    title: "Pesquisa fácil",
+    fadeType: "fade-up",
+  },
+];
+const changeIMGS = [
+  "/images/illustration-features-tab-1.svg",
+  "/images/illustration-features-tab-2.svg",
+  "/images/illustration-features-tab-3.svg",
+];
+const changeCONTENT = [
+  {
+    title: "Favoritos em 1 clique",
+    desc: "Organize seus favoritos como quiser. Nossa simples interface de arrastar e soltar oferece controle total sobre como você gerencia seus sites preferidos.",
+  },
+  {
+    title: "Pesquisa inteligente",
+    desc: "Nosso poderoso recurso de pesquisa ajudará você a encontrar seus sites salvos num instante. Não há mais a necessidade de vasculhar por todos os seus favoritos.",
+  },
+  {
+    title: "Compartilhe os seus favoritos",
+    desc: "Compartilhe facilmente seus favoritos e coleções com outras pessoas. Crie um link compartilhável para enviá-lo com o clique de um botão.",
+  },
+];
 
-  const changeToSimpleBookmarking = () => {
-    setSBActivated(true);
-    setESActivated(false);
-    setSSActivated(false);
-  };
-  const changeToSpeedySearch = () => {
-    setSBActivated(false);
-    setESActivated(false);
-    setSSActivated(true);
-  };
-  const changeToEasySharing = () => {
-    setSBActivated(false);
-    setESActivated(true);
-    setSSActivated(false);
-  };
+function Features() {
+  const [indexBTN, setIndexBTN] = useState(0);
 
   return (
     <>
-      <section className="features-container" id={"features"}>
+      <section className="features" id="features">
         <div
-          className="features-texts-container"
+          className="features__texts"
           data-aos="fade-up"
           data-aos-offset="200"
         >
+          {/* SECTION TITLE */}
           <h2>Recursos</h2>
           <p>
-            Nosso objetivo é facilitar e agilizar o acesso ao seu sites favoritos. Seus favoritos são sincronizados entre seus dispositivos para que você possa acessá-los em qualquer lugar.
+            Nosso objetivo é facilitar e agilizar o acesso ao seu sites
+            favoritos. Seus favoritos são sincronizados entre seus dispositivos
+            para que você possa acessá-los em qualquer lugar.
           </p>
         </div>
-        <div className="features-list-container">
-          <ul className="features-list">
-            <li data-aos="fade-right">
-              <button
-                onClick={changeToSimpleBookmarking}
-                className={`features-list-item ${
-                  SBactivated ? "active" : null
-                }`}
-              >
-                Simple Bookmarking
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={changeToSpeedySearch}
-                className={`features-list-item ${
-                  SSactivated ? "active" : null
-                }`}
-              >
-                Pesquisa rápida
-              </button>
-            </li>
-            <li data-aos="fade-up">
-              <button
-                onClick={changeToEasySharing}
-                className={`features-list-item ${
-                  ESactivated ? "active" : null
-                }`}
-              >
-                Pesquisa fácil
-              </button>
-            </li>
-            <div className="marker"></div>
+
+        <div className="features__change">
+          {/* CHANGE CONTENT BUTTONS (3) */}
+          <ul className="features__change__items">
+            {changeBTNS.map((item, index) => (
+              <li data-aos={item.fadeType} key={index}>
+                <button
+                  onClick={() => setIndexBTN(index)}
+                  className={`features__change__items__item ${
+                    index === indexBTN ? "active" : null
+                  }`}
+                >
+                  {item.title}
+                </button>
+              </li>
+            ))}
           </ul>
 
-          <div className="features-bookmark-container">
+          <div className="features__change__content">
+            {/* IMGS (3) */}
             <div
-              className="bookmark-img-wrapper"
+              className="features__change__content__imgWrapper"
               data-aos="fade-right"
               data-aos-offset="220"
             >
-              {SBactivated && (
-                <>
-                  <img
-                    src="/images/illustration-features-tab-1.svg"
-                    alt="Bookmark"
-                  />
-                </>
-              )}
-
-              {SSactivated && (
-                <img
-                  src="/images/illustration-features-tab-2.svg"
-                  alt="Bookmark"
-                />
-              )}
-
-              {ESactivated && (
-                <img
-                  src="/images/illustration-features-tab-3.svg"
-                  alt="Bookmark"
-                />
-              )}
+              <img src={changeIMGS[indexBTN]} alt="Bookmark" />
             </div>
 
+            {/* CONTENT (3) */}
             <div
-              className="bookmark-texts-container"
+              className="features__change__content__texts"
               data-aos="fade-up"
               data-aos-offset="220"
             >
-              {SBactivated && (
-                <>
-                  <h2>Favoritos em 1 clique</h2>
-                  <p>
-                    Organize seus favoritos como quiser. Nossa simples
-                    interface de arrastar e soltar oferece controle total sobre como
-                    você gerencia seus sites preferidos.
-                  </p>
-                </>
-              )}
+              <h2>{changeCONTENT[indexBTN].title}</h2>
+              <p>{changeCONTENT[indexBTN].desc}</p>
 
-              {SSactivated && (
-                <>
-                  <h2>Pesquisa inteligente</h2>
-                  <p>
-                    Nosso poderoso recurso de pesquisa ajudará você a encontrar seus sites salvos
-                    num instante. Não há mais a necessidade de vasculhar por todos os seus favoritos.
-                  </p>
-                </>
-              )}
-
-              {ESactivated && (
-                <>
-                  <h2>Compartilhe os seus favoritos</h2>
-                  <p>
-                    Compartilhe facilmente seus favoritos e coleções com outras pessoas.
-                    Crie um link compartilhável para enviá-lo com o clique de um botão.
-                  </p>
-                </>
-              )}
+              {/* ADD EXTENSION BTN */}
               <Link to="#download">
-                <button className="features-add-btn">ADICIONAR</button>
+                <button className="features__change__content__texts__addBTN">
+                  ADICIONAR
+                </button>
               </Link>
             </div>
           </div>
