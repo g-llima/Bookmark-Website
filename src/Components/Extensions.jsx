ed40f48329331521b53b52aabc002a4dec2a536b
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-20 18:17:49 -0300

    Refactored Extensions.jsx

--- src/Components/Extensions.jsx
+++ src/Components/Extensions.jsx
@@ -1,59 +1,75 @@
 import React from "react";
 import "./Styles/Extensions.css";
 
+const browserCards = [
+  {
+    img: "/images/logo-chrome.svg",
+    link: "https://chrome.google.com/webstore/detail/bookmark-sidebar/jdbnofccmhefkmjbkkdkfiicjkgofkdh",
+    text: "Adicionar ao Chrome",
+    verMin: "62",
+    flipType: "up",
+    offset: "400",
+    delay: "0",
+  },
+  {
+    img: "/images/logo-firefox.svg",
+    link: "https://addons.mozilla.org/pt-BR/firefox/addon/bookmarks-organizer/",
+    text: "Adicionar ao Firefox",
+    verMin: "55",
+    flipType: "down",
+    offset: "400",
+    delay: "100",
+  },
+  {
+    img: "/images/logo-opera.svg",
+    link: "https://addons.opera.com/pt-br/extensions/details/v7-bookmarks/",
+    text: "Adicionar ao Opera",
+    verMin: "46",
+    flipType: "up",
+    offset: "400",
+    delay: "200",
+  },
+];
+
 function Extensions() {
   return (
     <>
-      <section className="extensions-container" id="download">
-        <div className="extensions-texts-container" data-aos="fade-down">
+      <section className="extensions" id="download">
+        {/* SECTION TEXTS */}
+        <div className="extensions__texts" data-aos="fade-down">
           <h2>Adicione a extensão</h2>
           <p>
-            Temos mais navegadores na mesa de produção. Por favor, avise-nos caso você tenha um favorito que gostaria que priorizermos.
+            Temos mais navegadores na mesa de produção. Por favor, avise-nos
+            caso você tenha um favorito que gostaria que priorizermos.
           </p>
         </div>
-        <ul className="extensions-cards-container">
-          <li data-aos="flip-up" data-aos-offset="400">
-            <a
-              href="https://chrome.google.com/webstore/detail/bookmark-sidebar/jdbnofccmhefkmjbkkdkfiicjkgofkdh"
-              target="_blank"
-              className="extensions-card-item"
+
+        {/* BROWSER CARDS ITEMS */}
+        <ul className="extensions__cards">
+          {browserCards.map((item, index) => (
+            <li
+              key={index}
+              className="extensions__cards__card"
+              data-aos={`flip-${item.flipType}`}
+              data-aos-offset={item.offset}
+              data-aos-delay={item.delay}
+              style={{ top: index * 2.5 + "rem" }}
             >
-              <img className="card-logo-img" src="/images/logo-chrome.svg" />
-              <h3>Adicionar ao Chrome</h3>
-              <p>Versão mínima: 62</p>
-              <img src="/images/bg-dots.svg" className="dots" />
-              <button className="btn-card-item">Adicionar & Instalar Extensão</button>
-            </a>
-          </li>
-          <li data-aos="flip-down" data-aos-offset="400" data-aos-delay="100">
-            <a
-              href="https://addons.mozilla.org/pt-BR/firefox/addon/bookmarks-organizer/"
-              target="_blank"
-              className="extensions-card-item firefox"
-            >
-              <img
-                className="card-logo-imgfire"
-                src="/images/logo-firefox.svg"
-              />
-              <h3>Adicionar ao Firefox</h3>
-              <p>Versão mínima: 55</p>
-              <img src="/images/bg-dots.svg" className="dots" />
-              <button className="btn-card-item">Adicionar & Instalar Extensão</button>
-            </a>
-          </li>
-          <li data-aos="flip-up" data-aos-offset="400" data-aos-delay="200">
-            <a
-              href="https://addons.opera.com/pt-br/extensions/details/v7-bookmarks/"
-              target="_blank"
-              className="extensions-card-item opera"
-            >
-              <img className="card-logo-img" src="/images/logo-opera.svg" />
-              <h3>Adicionar ao Opera</h3>
-              <p>Versão mínima: 46</p>
-              <img src="/images/bg-dots.svg" className="dots" />
-              <button className="btn-card-item">Adicionar & Instalar Extensão</button>
-            </a>
-          </li>
+              <a
+                href={item.link}
+                target="_blank"
+                className="extensions__cards__card__content"
+              >
+                <img className="card-logo-img" src={item.img} />
+                <h3>{item.text}</h3>
+                <p>Versão mínima: {item.verMin}</p>
+                <img src="/images/bg-dots.svg" className="dots" />
+                <button className="extensions__cards__card__content__btn">
+                  Adicionar & Instalar Extensão
+                </button>
+              </a>
+            </li>
+          ))}
         </ul>
       </section>
     </>
